34df221b6bd3e48fdf32b71d421be19c15ae61a3
Alexandra Staszewski <alexandra_sta@yahoo.de>
2024-09-12 12:34:31 +0200

    nav bar stylen

--- page.php
+++ page.php
@@ -12,30 +12,39 @@
             <p>Dies ist das Page Template</p>
             <h1><?php bloginfo('title'); ?></h1>
 
-            <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-        </div>
+            <nav class="nav-bar main-nav">
+            <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+            </div>
+            </nav>
+            
     </header>
 
     <main>
         <div class="container">
             <?php
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-    ?>
+                if ( have_posts() ) :
+                    while ( have_posts() ) : the_post();
+            ?>
 
-            <h2 class="my-post-title"><?php the_title(); ?></h2>
+            <h2 class="my-post-title">
+                <?php the_title(); ?>
+            </h2>
 
             <?php
-        the_content();
-    endwhile;
-endif;
-?>
+                    the_content();
+                endwhile;
+            endif;
+            ?>
         </div>
     </main>
 
     <footer>
         <div class="container">
-                       <?php wp_nav_menu( array( 'theme_location' => 'secondary-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+            <nav class="nav-bar footer-nav">
+            <?php wp_nav_menu( 
+                        array( 'theme_location' => 'secondary-menu') ); ?>
+            </nav>
+                       
             <p>© 2024 Enno Hyttrek</p>
         </div>
     </footer>
